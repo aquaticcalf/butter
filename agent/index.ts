c040dee960a0d1b647fa92dd6381857a0a1f143f
sam <aquaticcalf@proton.me>
2026-06-06 22:43:17 +0000

    pi

---
+++ agent/index.ts
@@ -0,0 +1,12 @@
+export { createAgent } from './src/agent.js'
+export type {
+  ToolParam,
+  ToolDef,
+  ModelConfig,
+  SessionConfig,
+  AgentConfig,
+  AgentEvent,
+  SessionInfo,
+  ImageAttachment,
+  AgentHandle,
+} from './src/types.js'
